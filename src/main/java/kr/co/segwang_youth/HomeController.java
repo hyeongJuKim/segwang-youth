340f002e9ba3d8688aa4a660bae82c3aa5c9b40b
hj <gudwndhkd@nate.com>
2017-03-31 23:12:07 +0900

    init Project

---
+++ src/main/java/kr/co/segwang_youth/HomeController.java
@@ -0,0 +1,20 @@
+package kr.co.segwang_youth;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by hj on 2017. 3. 31..
+ */
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String Home(){
+
+        return "home";
+    }
+
+}
